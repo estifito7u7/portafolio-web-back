5cefecf856046ad74280e3ef23dff44aafdc9ca9
Michael <estifitohistories12@gmail.com>
2025-02-19 00:50:20 -0600

    test 2

--- my_briefcase_back/settings.py
+++ my_briefcase_back/settings.py
@@ -23,14 +23,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 
-STORAGES = {
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
-
-
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 

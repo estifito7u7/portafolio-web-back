38991cee0b319bfb2458690de815d6245cc9a290
Michael <estifitohistories12@gmail.com>
2025-02-18 23:45:28 -0600

    solution errors v5

--- my_briefcase_back/settings.py
+++ my_briefcase_back/settings.py
@@ -99,12 +99,12 @@ DATABASES = {
 
 
 
-# STORAGES = {
-#     # ...
-#     "staticfiles": {
-#         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-#     },
-# }
+STORAGES = {
+    # ...
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 

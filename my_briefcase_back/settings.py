82b08a1bedb16fb384b258e1f8f416740b2b0547
Michael <estifitohistories12@gmail.com>
2025-02-18 21:00:02 -0600

    allowed host solution v2

--- my_briefcase_back/settings.py
+++ my_briefcase_back/settings.py
@@ -29,6 +29,8 @@ DEBUG = False
 ALLOWED_HOSTS = ["127.0.0.1","localhost" , "https://michaelfernadez.netlify.app" , "https://web-production-cdb9.up.railway.app"]
 
 
+CSRF_TRUSTED_ORIGINS = ['https://web-production-cdb9.up.railway.app' ,'"https://michaelfernadez.netlify.app"']
+
 # Application definition
 
 
